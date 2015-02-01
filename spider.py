4000ea8d417c716689689829c3367fd42d0c4b6a
yancankang <674810893@qq.com>
2015-02-01 23:04:17 +0800

    加入文件分割，提取全局变量

--- spider.py
+++ spider.py
@@ -6,13 +6,14 @@ import urllib
 from http.client import HTTPConnection
 from collections import deque
 import re
-
+import os
+import globalValues
 # 开启debug模式
-#HTTPConnection.debuglevel=1
-#处理消息头和消息体，消息体需要编码为二进制，消息头utf-8
+# HTTPConnection.debuglevel=1
+# 处理消息头和消息体，消息体需要编码为二进制，消息头utf-8
 indexUrl = 'http://5sing.kugou.com/login/'
 
-#http://5sing.kugou.com/my
+# http://5sing.kugou.com/my
 loginData = {'RefUrl': 'http://5sing.kugou.com/index.html', 'txtUserName': 'ycktest1', 'txtPassword': 'ycktest1',
              'txtCheckCode': '验证码'}
 loginData = urllib.parse.urlencode(loginData).encode('utf-8')
@@ -51,21 +52,26 @@ def openUrl():
 
     try:
         request = urllib.request.Request(openingUrl)
-        response = urllib.request.urlopen(request,timeout=10)
-        html = response.read().decode('utf-8', 'ignore')  #.encode('utf-8','ignore').decode('utf-8')
+        response = urllib.request.urlopen(request, timeout=10)
+        html = response.read().decode('utf-8', 'ignore')
     except Exception as e:
         print('%s 抓取失败' % (openingUrl))
         print(e)
         return
+    finally:
+        visited.add(openingUrl)
+
     for urlChild in linkRe.findall(html):
         urlChild not in visited and urlQueue.append(urlChild)
     count += 1
-    #with open('temp/' + str(count) + '.html', 'w', encoding='utf-8') as file:
+    # with open('temp/' + str(count) + '.html', 'w', encoding='utf-8') as file:
     #    file.write(html)
     return html
 
 
-keys=(('演唱：','原唱：'),('演唱：','作词：','作曲：','编曲：'))
+
+
+
 def buildSongMsg(html):
     '''
     从抓取的网页中，提取有用信息，并且拼接为一行
@@ -73,7 +79,7 @@ def buildSongMsg(html):
     :return:
     '''
     try:
-        return {x:getWord(x,html) for x in keys[getSongType(html)]}
+        return {x: getWord(x, html) for x in globalValues.keys[getSongType(html)]}
     except:
         return None
 
@@ -101,46 +107,58 @@ def getWord(word, html):
     start = html.find(word)
     word_start = html.find('title=\"', start, start + 500) + 7
     word_end = html.find('\"', word_start, word_start + 50)
-    if start==-1 or word_start==6 or word_end==-1:
+    if start == -1 or word_start == 6 or word_end == -1:
         word_end = html.find('</a>', start, start + 500)
-        word_start=html.rfind('>',0,word_end)+1
-    if start==-1 or word_start==-1 or word_end==-1:
-        word=None
+        word_start = html.rfind('>', 0, word_end) + 1
+    if start == -1 or word_start == -1 or word_end == -1:
+        word = None
     else:
-        word=html[word_start:word_end]
+        word = html[word_start:word_end]
     return word
 
+
 def getSongType(html):
-    if  html.find('原唱：') !=-1 and  html.find('演唱：')!=-1:
+    if html.find('原唱：') != -1 and html.find('演唱：') != -1:
         return 0
-    elif html.find('混缩：') !=-1 and  html.find('演唱：')!=-1:
+    elif html.find('混缩：') != -1 and html.find('演唱：') != -1:
         return 1
 
+
 def getSongName(html):
-    start=html.find('class=\"view_tit\"')
-    word_start=html.find('<h1>',start,start+500)+4
-    word_end=html.find('</h1>',start,start+500)
-    if start==-1 or word_start==3 or word_end==-1:
+    start = html.find('class=\"view_tit\"')
+    word_start = html.find('<h1>', start, start + 500) + 4
+    word_end = html.find('</h1>', start, start + 500)
+    if start == -1 or word_start == 3 or word_end == -1:
         return None
     else:
         return html[word_start:word_end]
 
+
 def writeSongMsg(html):
-    print('写入歌曲信息')
-    with open('result.txt',encoding='utf-8',errors='ignore',mode='a') as result_file:
-        songName=getSongName(html)
-        msg=buildSongMsg(html)
-        songName and msg and result_file.write((songName+msg.__str__()+'\n\n'))
+    songName = getSongName(html)
+    msg = buildSongMsg(html)
+    if songName and msg:
+        globalValues.SONG_MSG_BUFFER = globalValues.SONG_MSG_BUFFER + songName + msg.__str__() + '\n\n'
+        if len(globalValues.SONG_MSG_BUFFER) > globalValues.BUFFER_SIZE:
+            filePath = 'result/songs' + str(globalValues.FILE_INDEX) + '.txt'
+            with open(filePath, encoding='utf-8', errors='ignore', mode='a') as result_file:
+                print('写入歌曲信息')
+                result_file.write(globalValues.SONG_MSG_BUFFER)
+                globalValues.SONG_MSG_BUFFER = ''
+            if (os.path.getsize(filePath) > globalValues.FILE_SIZE):
+                globalValues.FILE_INDEX += 1
 
-login()
 
-while urlQueue:
-    html=openUrl()
-    if(html==None):
-        continue
-    writeSongMsg(html)
+if __name__=='__main__':
+    login()
 
-print(count)
+    while urlQueue:
+        html = openUrl()
+        if (html == None):
+            continue
+        writeSongMsg(html)
+
+    print(count)
 
 
 
