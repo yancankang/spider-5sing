33ff41d0c96f190c5a168226826df48d53349751
yancankang <674810893@qq.com>
2015-02-01 17:04:25 +0800

    初步提取信息

--- spider.py
+++ spider.py
@@ -51,32 +51,38 @@ def openUrl():
 
     try:
         request = urllib.request.Request(openingUrl)
-        response = urllib.request.urlopen(request)
+        response = urllib.request.urlopen(request,timeout=10)
         html = response.read().decode('utf-8', 'ignore')  #.encode('utf-8','ignore').decode('utf-8')
     except Exception as e:
         print('%s 抓取失败' % (openingUrl))
         print(e)
         return
-
     for urlChild in linkRe.findall(html):
-       urlChild not in visited and urlQueue.append(urlChild)
+        urlChild not in visited and urlQueue.append(urlChild)
     count += 1
+    #with open('temp/' + str(count) + '.html', 'w', encoding='utf-8') as file:
+    #    file.write(html)
+    return html
 
-    with open('temp/' + str(count) + '.html', 'w', encoding='utf-8') as file:
-        file.write(html)
 
-    with open('result.txt',encoding='utf-8',errors='ignore',mode='a') as result_file:
+keys=(('演唱：','原唱：'),('演唱：','作词：','作曲：','编曲：'))
+def buildSongMsg(html):
+    '''
+    从抓取的网页中，提取有用信息，并且拼接为一行
+    :param html:
+    :return:
+    '''
+    try:
+        return {x:getWord(x,html) for x in keys[getSongType(html)]}
+    except:
+        return None
 
-        result_file.write(getWord('演唱：'))
-        result_file.write(getWord('作词：'))
-        result_file.write(getWord('作曲：'))
-        result_file.write(getWord('编曲：'))
 
-def buildMusicMsg(html):
-    singer=
-
-#进行登录
 def login():
+    '''
+    登陆
+    :return:
+    '''
     openingUrl = urlQueue.popleft()
     print('正在访问网页：%s' % (openingUrl))
     request = urllib.request.Request(openingUrl, loginData)
@@ -88,16 +94,51 @@ def login():
 
 
 def getWord(word, html):
+    '''
+    获取演唱，作词等信息
+    :return 演唱等
+    '''
     start = html.find(word)
     word_start = html.find('title=\"', start, start + 500) + 7
     word_end = html.find('\"', word_start, word_start + 50)
-    return html[word_start:word_end]
+    if start==-1 or word_start==6 or word_end==-1:
+        word_end = html.find('</a>', start, start + 500)
+        word_start=html.rfind('>',0,word_end)+1
+    if start==-1 or word_start==-1 or word_end==-1:
+        word=None
+    else:
+        word=html[word_start:word_end]
+    return word
 
+def getSongType(html):
+    if  html.find('原唱：') !=-1 and  html.find('演唱：')!=-1:
+        return 0
+    elif html.find('混缩：') !=-1 and  html.find('演唱：')!=-1:
+        return 1
+
+def getSongName(html):
+    start=html.find('class=\"view_tit\"')
+    word_start=html.find('<h1>',start,start+500)+4
+    word_end=html.find('</h1>',start,start+500)
+    if start==-1 or word_start==3 or word_end==-1:
+        return None
+    else:
+        return html[word_start:word_end]
+
+def writeSongMsg(html):
+    print('写入歌曲信息')
+    with open('result.txt',encoding='utf-8',errors='ignore',mode='a') as result_file:
+        songName=getSongName(html)
+        msg=buildSongMsg(html)
+        songName and msg and result_file.write((songName+msg.__str__()+'\n\n'))
 
 login()
 
 while urlQueue:
-    openUrl()
+    html=openUrl()
+    if(html==None):
+        continue
+    writeSongMsg(html)
 
 print(count)
 
