168eaa794deb2f932f5d96b6e00944f97a60156e
yancankang <674810893@qq.com>
2015-02-02 23:42:08 +0800

    修复重复访问的bug

--- spider.py
+++ spider.py
@@ -3,7 +3,6 @@ import urllib.request
 import http.cookiejar
 import urllib.parse
 import urllib
-from http.client import HTTPConnection
 from collections import deque
 import re
 import os
@@ -20,56 +19,49 @@ loginData = urllib.parse.urlencode(loginData).encode('utf-8')
 tempData = {'user': 'admin'}
 tempData = urllib.parse.urlencode(tempData).encode('utf-8')
 
-#header={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#'Accept-Language':'zh-CN,zh;q=0.8','User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'}
+# header={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+# 'Accept-Language':'zh-CN,zh;q=0.8','User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'}
 headers = [('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
            ('Accept-Language', 'zh-CN,zh;q=0.8'), ('User-Agent',
                                                    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36')]
 
 
-#增加cookie信息
+# 增加cookie信息
 cj = http.cookiejar.LWPCookieJar()
 cookie_support = urllib.request.HTTPCookieProcessor(cj)
 opener = urllib.request.build_opener(cookie_support, urllib.request.HTTPHandler)
 opener.addheaders = headers
 urllib.request.install_opener(opener)
 
-#初始化访问过的集合和待访问的url队列
+# 初始化访问过的集合和待访问的url队列
 visited = set()
 urlQueue = deque()
-count = 0
 
-#加入主页
+# 加入主页
 urlQueue.append(indexUrl)
 
 linkRe = re.compile('href="(.+?.html)"')
 
 
 def openUrl():
-    global count
     openingUrl = urlQueue.popleft()
-    print('%d.正在访问网页：%s.' % (count, openingUrl))
-
+    print('%d.正在访问网页：%s.' % (globalValues.SUCCESS_COUNT, openingUrl))
     try:
         request = urllib.request.Request(openingUrl)
         response = urllib.request.urlopen(request, timeout=10)
         html = response.read().decode('utf-8', 'ignore')
+        globalValues.SUCCESS_COUNT += 1
     except Exception as e:
         print('%s 抓取失败' % (openingUrl))
         print(e)
+        globalValues.FAIL_COUNT+=1
         return
     finally:
         visited.add(openingUrl)
-
     for urlChild in linkRe.findall(html):
-        urlChild not in visited and urlQueue.append(urlChild)
-    count += 1
-    # with open('temp/' + str(count) + '.html', 'w', encoding='utf-8') as file:
-    #    file.write(html)
+        urlChild not in visited and urlChild not in urlQueue and urlQueue.append(urlChild)
+
     return html
-
-
-
 
 
 def buildSongMsg(html):
@@ -79,7 +71,7 @@ def buildSongMsg(html):
     :return:
     '''
     try:
-        return {x: getWord(x, html) for x in globalValues.keys[getSongType(html)]}
+        return {x: getWord(x, html) for x in globalValues.KEYS[getSongType(html)]}
     except:
         return None
 
@@ -139,29 +131,21 @@ def writeSongMsg(html):
     msg = buildSongMsg(html)
     if songName and msg:
         globalValues.SONG_MSG_BUFFER = globalValues.SONG_MSG_BUFFER + songName + msg.__str__() + '\n\n'
+        globalValues.SONG_PAGE_COUNT+=1
         if len(globalValues.SONG_MSG_BUFFER) > globalValues.BUFFER_SIZE:
             filePath = 'result/songs' + str(globalValues.FILE_INDEX) + '.txt'
             with open(filePath, encoding='utf-8', errors='ignore', mode='a') as result_file:
-                print('写入歌曲信息')
+                print('写入歌曲信息, 成功访问的页面数：'+str(globalValues.SUCCESS_COUNT)+'。访问失败的页面数：'+str(globalValues.FAIL_COUNT)+'。正确的歌曲页面数：'+str(globalValues.SONG_PAGE_COUNT))
                 result_file.write(globalValues.SONG_MSG_BUFFER)
                 globalValues.SONG_MSG_BUFFER = ''
             if (os.path.getsize(filePath) > globalValues.FILE_SIZE):
                 globalValues.FILE_INDEX += 1
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     login()
-
     while urlQueue:
         html = openUrl()
         if (html == None):
             continue
         writeSongMsg(html)
-
-    print(count)
-
-
-
-
-
-
+    print(globalValues.SUCCESS_COUNT)
